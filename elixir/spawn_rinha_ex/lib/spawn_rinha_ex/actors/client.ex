0b69515ba7e7b683d476efc933074691b55bb271
Adriano Santos <sleipnir@bsd.com.br>
2024-02-07 19:31:02 -0300

    Rinha project structure

--- elixir/spawn_rinha_ex/lib/spawn_rinha_ex/actors/client.ex
+++ elixir/spawn_rinha_ex/lib/spawn_rinha_ex/actors/client.ex
@@ -1,12 +1,49 @@
 defmodule SpawnRinhaEx.Actors.Client do
   @moduledoc """
+  Module for interacting with the Spawn Rinha system as a client.
+
+  This module provides functions for performing credit, debit, and statement operations.
+
+  ## Usage
+
+  1. Use the `credit/3` and `debit/3` functions to perform credit and debit transactions.
+  2. Utilize the `statement/1` function to retrieve account statements.
 
   """
   alias Io.Eigr.Spawn.Rinha.CreditMessage, as: Credit
   alias Io.Eigr.Spawn.Rinha.DebitMessage, as: Debit
 
+  @doc """
+  Guards against invalid ID ranges.
+
+  ### Parameters:
+
+  - `n` - The value to be checked.
+  - `min` - The minimum allowed value.
+  - `max` - The maximum allowed value.
+
+  """
   defguardp is_range(n, min, max) when n in min..max
 
+  @doc """
+  Performs a credit transaction.
+
+  ### Parameters:
+
+  - `id` - The client ID.
+  - `value` - The amount to be credited.
+  - `description` - A description of the credit transaction.
+
+  ### Returns:
+
+  - `{:error, :invalid_id}` if the ID is outside the valid range.
+  - Result of the credit transaction if the ID is valid.
+
+  ### Example:
+
+  ```elixir
+  Client.credit(3, 100, "Payment")
+  """
   def credit(id, value, description) when is_range(id, 1, 5), do: {:error, :invalid_id}
 
   def credit(id, value, description) do
@@ -17,6 +54,27 @@ defmodule SpawnRinhaEx.Actors.Client do
     )
   end
 
+  @doc """
+  Performs a debit transaction.
+
+  ### Parameters:
+
+      id - The client ID.
+      value - The amount to be debited.
+      description - A description of the debit transaction.
+
+  ### Returns:
+
+      {:error, :invalid_id} if the ID is outside the valid range.
+      Result of the debit transaction if the ID is valid.
+
+  ### Example:
+
+  elixir
+
+  Client.debit(2, 50, "Purchase")
+
+  """
   def debit(id, value, description) when is_range(id, 1, 5), do: {:error, :invalid_id}
 
   def debit(id, value, description) do
@@ -27,6 +85,22 @@ defmodule SpawnRinhaEx.Actors.Client do
     )
   end
 
+  @doc """
+  Retrieves the account statement for a client.
+  Parameters:
+
+      id - The client ID.
+
+  Returns:
+
+  No return value.
+  Example:
+
+  elixir
+
+  Client.statement(4)
+
+  """
   def statement(id) do
   end
 end
