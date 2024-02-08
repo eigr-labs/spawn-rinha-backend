56a3a7534b0e92e03be580379ecb3adf54d3c813
Adriano Santos <sleipnir@bsd.com.br>
2024-02-08 16:41:56 -0300

    Merge

--- elixir/spawn_rinha_ex/lib/spawn_rinha_ex/actors/client.ex
+++ elixir/spawn_rinha_ex/lib/spawn_rinha_ex/actors/client.ex
@@ -23,7 +23,7 @@ defmodule SpawnRinhaEx.Actors.Client do
   - `max` - The maximum allowed value.
 
   """
-  defguard is_range(n, min, max) when n in min..max
+  defguard is_range(n) when is_integer(n) and n > 0 and n < 6
 
   @doc """
   Performs a credit transaction.
@@ -46,7 +46,7 @@ defmodule SpawnRinhaEx.Actors.Client do
   ```
 
   """
-  def credit(id, _value, _description) when is_range(id, 1, 5), do: {:error, :invalid_id}
+  def credit(id, _value, _description) when is_range(id), do: {:error, :invalid_id}
 
   def credit(id, value, description) do
     SpawnSdk.invoke("#{id}",
