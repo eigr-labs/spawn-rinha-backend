41804eac8787abd0b299cba729f402bed3e29f31
Adriano Santos <sleipnir@bsd.com.br>
2024-02-09 20:34:49 -0300

    Adjusts

--- elixir/spawn_rinha_ex/config/runtime.exs
+++ elixir/spawn_rinha_ex/config/runtime.exs
@@ -2,3 +2,12 @@ import Config
 
 config :logger,
   level: :info
+
+config :mnesia,
+  dir:
+    to_charlist(
+      Path.join(
+        File.cwd!(),
+        "#{System.get_env("PROXY_DATABASE_DATA_DIR", "data")}/#{to_string(node())}"
+      )
+    )
